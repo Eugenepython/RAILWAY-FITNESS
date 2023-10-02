4b996fb679c60355145725abafa29b69b0b495b8
Eugenepython <50318257+Eugenepython@users.noreply.github.com>
2023-10-02 23:19:23 +0100

    changing PG_NAME to PG_DATABASE

--- Server/app.js
+++ Server/app.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 const pool = new Pool({
   user: process.env.PG_USER,
   host: process.env.PG_HOST,
-  database: process.env.PG_NAME,
+  database: process.env.PG_DATABASE, // THIS SHOULD COME AS  POSTGRES?
   password: process.env.PG_PASSWORD,
   port: process.env.PG_PORT,
 });
